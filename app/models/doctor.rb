09dbc5d5e9366e84d05a33dbacebf2151a312cf8
github username <github email address>
2020-08-05 17:47:03 -0400

    patient and doctor deliverables 8/5

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,25 +1,41 @@
 require 'pry'
 
 class Doctor
-    attr_writer :specialty
+    attr_reader :specialty
     attr_accessor :name, :years
+    @@all = []
 
     def initialize(name, specialty, years=1)
         @name = name
         @specialty = specialty
         @years = years
+        @@all << self
     end 
+
+    def self.find_by_specialty(specialty)
+        self.all.filter do |doctor|
+        doctor.specialty == specialty
+    end 
+end  
+
+    def self.all
+        @@all
+    end
 
     def say_name
         puts "Hi, my name is #{name}."
     end 
 
+    def say_greeting 
+        puts "It's so nice to see you. How are you feeling today?"
+    end
+
     def say_years
         puts "I have #{years} years in the medical field."
     end
+
 end
 
 # example invocation derek = Doctor.new("derek", "internal", 5)
-
 
 binding.pry
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,23 +1,44 @@
 require 'pry'
 
 class Patient
+    attr_reader :impatience
     attr_accessor :name, :age
+    @@all = []
 
     def initialize(name, age)
         @name = name
         @age = age
+        @impatience = 0
+        @@all << self
     end 
 
-    def say_name
-        puts "Hi, my name is #{name}."
-    end 
-
-    def say_age
-        puts "I am #{age} years old."
+    def self.all
+        @@all
     end
+
+    def inquire_appt_ready
+        puts "The doctor will be ready soon."
+        self.increase_impatience
+    end 
+
+    # I had this yesterday included in my code, it seems repetitve / unnecessary
+    #def say_name
+        #puts "Hi, my name is #{name}."
+    #end 
+
+    #def say_age
+        #puts "I am #{age} years old."
+    #end
+
+    private 
+
+    def increase_impatience
+        @impatience += 1
+    end 
+
 end
 
 
-madeline = Patient.new("madeline", 25)
+#example invocation madeline = Patient.new("madeline", 25)
 
 binding.pry
